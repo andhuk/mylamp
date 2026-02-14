f22bce7c013c18b1eabe39a703b21fad1d2f696d
Andrii Huk <50157110+andhuk@users.noreply.github.com>
2026-02-14 23:34:52 +0200

    0.1.5

--- random.js
+++ random.js
@@ -1,118 +1,101 @@
 (function () {
     'use strict';
 
-    var pluginSVG = '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z" fill="currentColor"/></svg>';
+    var network = new Lampa.Reguest();
+    var api_url = 'https://api.themoviedb.org/3/';
+    var api_key = '4ef66e126d2d9d59079589361de0f6ec';
 
-    var manifest = {
-        type: 'other',
-        version: '1.0.0',
-        name: 'Рандом',
-        description: 'Випадковий фільм або серіал',
-        component: 'random_movie'
-    };
+    function spin() {
+        Lampa.Select.show({
+            title: 'Рандомний вибір',
+            items: [
+                {title: 'Фільм', type: 'movie'},
+                {title: 'Серіал', type: 'tv'}
+            ],
+            onSelect: function (item) {
+                Lampa.Loading.start();
+                var page = Math.floor(Math.random() * 20) + 1;
+                
+                network.silent(
+                    api_url + 'discover/' + item.type + 
+                    '?api_key=' + api_key + 
+                    '&language=uk-UA' +
+                    '&sort_by=vote_count.desc' +
+                    '&vote_count.gte=200' +
+                    '&page=' + page,
+                    function (data) {
+                        Lampa.Loading.stop();
+                        if (data && data.results && data.results.length) {
+                            var movies = data.results;
+                            var topMovies = [];
+                            
+                            for (var i = 0; i < movies.length; i++) {
+                                if (movies[i].vote_average >= 6.5) {
+                                    topMovies.push(movies[i]);
+                                }
+                            }
+                            
+                            if (topMovies.length === 0) topMovies = movies;
+                            
+                            var movie = topMovies[Math.floor(Math.random() * topMovies.length)];
+                            
+                            Lampa.Activity.push({
+                                component: 'full',
+                                id: movie.id,
+                                method: item.type === 'movie' ? 'movie' : 'tv',
+                                card: movie
+                            });
+                        } else {
+                            Lampa.Noty.show('Помилка завантаження');
+                        }
+                    },
+                    function (error) {
+                        Lampa.Loading.stop();
+                        console.log('Random', 'API Error:', error);
+                        Lampa.Noty.show('Помилка API');
+                    }
+                );
+            }
+        });
+    }
 
     function startPlugin() {
-        Lampa.Manifest.plugins = manifest;
+        console.log('Random', 'Starting plugin...');
 
-        var network = new Lampa.Reguest();
-        var api_url = 'https://api.themoviedb.org/3/';
-        var api_key = '4ef66e126d2d9d59079589361de0f6ec';
-
-        function spin() {
-            Lampa.Select.show({
-                title: 'Рандомний вибір',
-                items: [
-                    {title: 'Фільм', type: 'movie'},
-                    {title: 'Серіал', type: 'tv'}
-                ],
-                onSelect: function (item) {
-                    Lampa.Loading.start();
-                    var page = Math.floor(Math.random() * 20) + 1;
-                    
-                    network.silent(
-                        api_url + 'discover/' + item.type + 
-                        '?api_key=' + api_key + 
-                        '&language=uk-UA' +
-                        '&sort_by=vote_count.desc' +
-                        '&vote_count.gte=200' +
-                        '&page=' + page,
-                        function (data) {
-                            Lampa.Loading.stop();
-                            if (data && data.results && data.results.length) {
-                                var movies = data.results;
-                                var topMovies = [];
-                                
-                                for (var i = 0; i < movies.length; i++) {
-                                    if (movies[i].vote_average >= 6.5) {
-                                        topMovies.push(movies[i]);
-                                    }
-                                }
-                                
-                                if (topMovies.length === 0) topMovies = movies;
-                                
-                                var movie = topMovies[Math.floor(Math.random() * topMovies.length)];
-                                
-                                Lampa.Activity.push({
-                                    component: 'full',
-                                    id: movie.id,
-                                    method: item.type === 'movie' ? 'movie' : 'tv',
-                                    card: movie
-                                });
-                            } else {
-                                Lampa.Noty.show('Помилка завантаження');
-                            }
-                        },
-                        function () {
-                            Lampa.Loading.stop();
-                            Lampa.Noty.show('Помилка API');
-                        }
-                    );
-                }
-            });
-        }
-
-        // Добавляем компонент в настройки
-        Lampa.SettingsApi.addComponent({
-            component: 'random_movie',
-            icon: pluginSVG,
-            name: 'Рандом'
-        });
-
-        // Добавляем кнопку запуска
-        Lampa.SettingsApi.addParam({
-            component: 'random_movie',
-            param: {
-                type: 'button'
-            },
-            field: {
-                name: 'Випадковий фільм',
-                description: 'Отримати випадковий фільм або серіал'
-            },
-            onChange: spin
-        });
-
-        // Добавляем кнопку в шапку
-        var timer = setInterval(function () {
-            var actions = $('.head__actions');
-            if (actions.length) {
-                clearInterval(timer);
+        // Додаємо пункт в головне меню навігації
+        var menuTimer = setInterval(function () {
+            var menu = $('.menu .menu__list');
+            if (menu.length) {
+                clearInterval(menuTimer);
+                console.log('Random', 'Menu found, adding item...');
                 
-                $('#head_random').remove();
-                var headButton = '<div id="head_random" class="head__action selector">' + pluginSVG + '</div>';
-                $('.head__actions').append(headButton);
+                // Видаляємо старий пункт якщо є
+                $('.menu__item[data-action="random"]').remove();
                 
-                $('#head_random').on('hover:enter hover:click hover:touch', function () {
+                // Створюємо новий пункт меню
+                var icon = '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z" fill="currentColor"/></svg>';
+                var menuItem = $('<li class="menu__item selector" data-action="random"><div class="menu__ico">' + icon + '</div><div class="menu__text">Рандом</div></li>');
+                
+                // Додаємо обробник події
+                menuItem.on('hover:enter', function() {
                     spin();
                 });
+                
+                // Вставляємо на перше місце
+                menu.prepend(menuItem);
+                
+                console.log('Random', 'Menu item added');
             }
         }, 100);
     }
 
     if (window.appready) {
+        console.log('Random', 'App already ready');
         startPlugin();
     } else {
         Lampa.Listener.follow('app', function (e) {
             if (e.type === 'ready') {
+                console.log('Random', 'App ready');
                 startPlugin();
             }
         });
