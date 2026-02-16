22cfbd5139a91efaf4ef28e7c008b3f343117e1d
Andrii Huk <50157110+andhuk@users.noreply.github.com>
2026-02-16 17:28:01 +0200

    Update random-movie.js

--- examples/random-movie.js
+++ examples/random-movie.js
@@ -3,55 +3,65 @@
 
     Lampa.Plugins.add('random_dice_recommender', function () {
         
-        // 1. Define the Button in Sidebar
+        // 1. Створюємо кнопку в меню
         Lampa.Main.add({
             id: 'random_dice',
             title: 'Random',
             icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM7 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm10 10c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm0-10c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zM7 19c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm5-5c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z" fill="white"/></svg>',
             onSelect: function () {
-                getRandomRecommendation();
+                startRandomSearch();
             }
         });
 
-        // 2. Data Retrieval Logic
-        function getRandomRecommendation() {
-            // Extract IDs from Lampa Storage
-            let history = Lampa.Storage.get('history', '[]');
-            let favorites = Lampa.Storage.get('favorite', '[]');
-            let rated = Lampa.Storage.get('items_vote', '[]');
-
-            // Merge and deduplicate source IDs
-            let sourcePool = [...history, ...favorites, ...rated].filter(item => item.id);
+        function startRandomSearch() {
+            // Безпечне отримання даних (якщо null - поверне [])
+            var history = Lampa.Storage.cache('history', 500, []);
+            var favorites = Lampa.Storage.cache('favorite', 500, []);
             
-            if (sourcePool.length === 0) {
-                Lampa.Noty.show('No data found in history or favorites.');
+            // Зливаємо докупи
+            var pool = [].concat(history, favorites);
+
+            // Жорстка фільтрація: прибираємо все, де немає ID або назви
+            var validPool = pool.filter(function(item) {
+                return item && item.id && (item.title || item.name);
+            });
+
+            if (validPool.length === 0) {
+                Lampa.Noty.show('Історія порожня. Подивись хоч щось!');
                 return;
             }
 
-            // Pick a random seed item
-            let seed = sourcePool[Math.floor(Math.random() * sourcePool.length)];
-            
-            fetchSimilar(seed);
+            // Рандомний вибір "насіння" для рекомендацій
+            var seed = validPool[Math.floor(Math.random() * validPool.length)];
+            fetchRecommendations(seed);
         }
 
-        // 3. API Call for Similar Content (TMDB Example)
-        function fetchSimilar(seed) {
-            let type = seed.type || 'movie';
-            let url = `https://api.themoviedb.org/3/${type}/${seed.id}/recommendations?api_key=${Lampa.TMDB.key()}&language=${Lampa.Storage.get('language','ru')}`;
+        function fetchRecommendations(seed) {
+            var type = seed.type || (seed.name ? 'tv' : 'movie');
+            var api_key = Lampa.TMDB.key();
+            var lang = Lampa.Storage.get('language', 'uk');
+            
+            // Формуємо URL до TMDB
+            var url = 'https://api.themoviedb.org/3/' + type + '/' + seed.id + '/recommendations?api_key=' + api_key + '&language=' + lang;
 
             Lampa.Loading.show();
 
+            // Використовуємо вбудований метод Lampa для обходу CORS (якщо треба) або звичайний fetch
             fetch(url)
-                .then(response => response.json())
-                .then(data => {
+                .then(function(response) { 
+                    if (!response.ok) throw new Error('Network response was not ok');
+                    return response.json(); 
+                })
+                .then(function(data) {
                     Lampa.Loading.hide();
+                    
                     if (data.results && data.results.length > 0) {
-                        let result = data.results[Math.floor(Math.random() * data.results.length)];
+                        var result = data.results[Math.floor(Math.random() * data.results.length)];
                         
-                        // Push to Full Info Screen
+                        // Відкриваємо картку знайденого фільму
                         Lampa.Activity.push({
                             url: '',
-                            title: 'Full Info',
+                            title: 'Подробиці',
                             component: 'full',
                             id: result.id,
                             method: type,
@@ -59,12 +69,12 @@
                             source: 'tmdb'
                         });
                     } else {
-                        Lampa.Noty.show('Could not find similar items.');
+                        Lampa.Noty.show('Схожого контенту не знайдено.');
                     }
                 })
-                .catch(() => {
+                .catch(function(err) {
                     Lampa.Loading.hide();
-                    Lampa.Noty.show('Network Error');
+                    Lampa.Noty.show('Помилка запиту: ' + err.message);
                 });
         }
     });
